0cb803c7dd245eca4957bbcbae82e476b2df4472
sardor93-github <akramovsardor060393@gmail.com>
2023-05-07 20:14:38 +0900

    feat: building express web server

--- server.js
+++ server.js
@@ -1,5 +1,33 @@
-const moment = require("moment");
+console.log("Web Serverni boshlash");
+const express = require("express");
+const app = express();
+const http = require("http");
 
-const time = moment().format("YYYY-MM-DD HH:mm");
+//1 Kirish code
 
-console.log("Hozirgi vaqt:", time);
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+//2: Session code
+
+//3: Views code
+
+app.set("views", "views");
+app.set("view engine", "ejs");
+
+//4: Routing code
+
+app.get("/", function(req, res){
+    res.end("<h1>HELLO WORLD</h1>");
+});
+
+app.get("/hello", function(req, res){
+    res.end("<h1>HELLO WORLD by Robert</h1>");
+});
+
+const server = http.createServer(app);
+let PORT = 3000;
+server.listen(PORT, function(){
+    console.log(`The server is running successfully on port: ${PORT}`);
+});
