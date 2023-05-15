1a11295e2fe32d057e8ae36cfc230d8eda39d680
sardor93-github <akramovsardor060393@gmail.com>
2023-05-15 23:59:54 +0900

    feat: CRUD operation

--- app.js
+++ app.js
@@ -35,17 +35,27 @@ app.set("view engine", "ejs");
 
 //4: Routing code
 app.post("/create-item", (req, res) => {
-    //TODO: code with DB here 
+    console.log("user entered /create-item");
+    const new_reja  = req.body.reja;
+    db.collection("plans").insertOne({reja: new_reja}, (err, data) => {
+        if (err){
+            console.log(err);
+            res.end("Something went wrong");
+        } else {
+            res.end("successfully added");
+        }
+    });
+    // res.end("success");    
 });
 
 app.get("/", function (req, res) {
+    console.log('user entered /');
     db.collection("plans").find().toArray((err, data) => {
         if (err) {
             console.log(err);
             res.end("something went wrong!!!");
         } else {
-            console.log(data);
-            res.render("reja");
+            res.render("reja", {items: data});
         }
     });
 });
