96f88ffa66e9ec8ffb80ebb80e9a768112d328ef
sardor93-github <akramovsardor060393@gmail.com>
2023-05-18 20:54:45 +0900

    fix: sintaksis

--- public/browser.js
+++ public/browser.js
@@ -77,7 +77,7 @@ document.addEventListener("click", function (e) {
   }
 });
 
-document.getElementById("clean-all").addEventListener("click", function () {
+document.getElementById("delete-all").addEventListener("click", function () {
   axios
     .post("/delete-all", { delete_all: true })
     .then((respose) => {
