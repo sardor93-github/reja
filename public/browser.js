d442a4515d7db0a6dd6571fd70fc7e90f6bcc717
sardor93-github <akramovsardor060393@gmail.com>
2023-05-20 13:59:27 +0900

    fix: sintaksis

---
+++ public/browser.js
@@ -0,0 +1,87 @@
+console.log("FrontEnd JS ishga tushdi!!!");
+
+// const axios = require('axios/dist/node/axios.cjs');
+
+function itemTemplate(item) {
+  return ` <li class="list-group-item list-group-item-info d-flex align-items-center justify-content-between">
+  <span class="item-text">
+    ${item.reja}
+  </span>
+  <div>
+    <button data-id="${item._id}" class="edit-me btn btn-secondary btn-sm mr-1">
+      Ozgartirish
+    </button>
+    <button data-id="${item._id}" class="delete-me btn btn-danger btn-sm">Ochirish</button>
+  </div>
+</li>`;
+}
+
+let createField = document.getElementById("create-field");
+
+document.getElementById("create-form").addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  axios
+    .post("/create-item", { reja: createField.value })
+    .then((response) => {
+      document
+        .getElementById("item-list")
+        .insertAdjacentHTML("beforeend", itemTemplate(response.data));
+      createField.value = "";
+      createField.focus();
+    })
+    .catch((err) => {
+      console.log("Iltimos qaytadan urinib ko'ring");
+    });
+});
+
+
+document.addEventListener("click", function (e) {
+  //delete operation
+  console.log(e.target);
+  if (e.target.classList.contains("delete-me")) {
+    if (confirm("Aniq ochirirmoqchimisiz???")) {
+      axios
+        .post("/delete-item", { id: e.target.getAttribute("data-id") })
+        .then((response) => {
+          console.log(response.data);
+          e.target.parentElement.parentElement.remove();
+        })
+        .catch((err) => {
+          console.log("Iltimos qaytadan urinib ko'ring!!!");
+        });
+    }
+  }
+
+  //edit operation
+  if (e.target.classList.contains("edit-me")) {
+    let userInput = prompt(
+      "O'zgartirish kiriting!!!",
+      e.target.parentElement.parentElement.querySelector(".item-text").innerHTML
+    );
+    if (userInput) {
+      axios
+        .post("/edit-item", {
+          id: e.target.getAttribute("data-id"),
+          new_input: userInput,
+        })
+        .then((response) => {
+          console.log(response.data);
+          e.target.parentElement.parentElement.querySelector(".item-text")
+            .innerHTML = userInput;
+        })
+        .catch((err) => {
+          console.log("Iltimos qaytadan urinib ko'ring!!!");
+        });
+    }
+  }
+});
+
+document.getElementById("delete-all").addEventListener("click", function () {
+  axios
+    .post("/delete-all", { delete_all: true })
+    .then((respose) => {
+      alert(respose.data.state);
+      document.location.reload();
+    });
+});
